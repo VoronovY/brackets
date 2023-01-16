638e5fb29b08bd1f36b7f3b716e6c2de8f2c3ba6
VoronovY <voronov.yuri87@gmail.com>
2023-01-16 16:52:54 +0100

    feat: brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  const hash = {};
+  const stack = [];
+
+  for (let [openBracket, closedBracket] of bracketsConfig) {
+    hash[openBracket] = closedBracket;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      (hash[str[i]] === str[i] && !stack.includes(str[i])) ||
+      (hash[str[i]] && hash[str[i]] !== str[i])
+    ) {
+      stack.push(hash[str[i]]);
+    } else {
+      const lastEl = stack.pop();
+      if (lastEl !== str[i]) return false;
+    }
+  }
+
+  return stack.length === 0;
+};
